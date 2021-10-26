c62ebebb88277477d7d184febec01cf4e1c6b215
Ivan <zeesnippi@gmail.com>
2021-10-26 18:24:32 +0300

    Some translations fixed.

--- dot_circle/src/layout/about/link_card/link_card.jsx
+++ dot_circle/src/layout/about/link_card/link_card.jsx
@@ -1,53 +1,42 @@
 import style from './link_card.module.css'
 import {withNamespaces} from "react-i18next";
-import i18next from 'i18next';
 import img1 from '../../../assets/item1_link_card.png'
 import img2 from '../../../assets/iitem2_link_card.png'
 import img3 from '../../../assets/iitem3_link_card.png'
 import img4 from '../../../assets/item4_link_card.png'
 import {NavLink} from "react-router-dom";
 
-const LinkCard = (props) => {
-    let t = (key) => i18next.t(key);
+const LinkCard = ({t}) => {
     return (
 
         <div className={style.container}>
-
 
             <div className={style.box}>
                 <img src={img1} alt=""/>
                 <p>{t('Dream team')}</p>
                 <hr/>
-                <NavLink to={'/useful'}>
-                    <span>{t('GO TO')}</span>
-                </NavLink>
+                <NavLink to={'/useful'}><span>{t('Go to')}</span></NavLink>
             </div>
 
             <div className={style.box}>
                 <img src={img2} alt=""/>
                 <p>{t('Useful')}</p>
                 <hr/>
-                <NavLink to={'/'}>
-                    <span>{t('GO TO')}</span>
-                </NavLink>
+                <NavLink to={'/'}><span>{t('Go to')}</span></NavLink>
             </div>
 
             <div className={style.box}>
                 <img src={img3} alt=""/>
                 <p>{t('Webinars')}</p>
                 <hr/>
-                <NavLink to={'/'}>
-                    <span >{t('GO TO')}</span>
-                </NavLink>
+                <NavLink to={'/'}><span >{t('Go to')}</span></NavLink>
             </div>
 
             <div className={style.box}>
                 <img src={img4} alt=""/>
                 <p>{t('Gallery')}</p>
                 <hr/>
-                <NavLink to={'/'}>
-                    <span>{t('GO TO')}</span>
-                </NavLink>
+                <NavLink to={'/'}><span>{t('Go to')}</span></NavLink>
             </div>
 
         </div>
