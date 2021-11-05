9415a9c05f18e3d50fb6eb0ee09116792189ad1a
Ivan <zeesnippi@gmail.com>
2021-11-05 15:01:56 +0200

    Useful component code refactoring

--- dot_circle/src/layout/useful/useful.jsx
+++ dot_circle/src/layout/useful/useful.jsx
@@ -1,82 +1,71 @@
 import s from "./useful.module.scss"
-import {useState} from "react";
-import i18next from "i18next";
 import {withNamespaces} from "react-i18next";
-import HeaderUsful from "./header/header";
+import faq from "../../assets/FAQ.png";
 
 
-const Useful = () => {
-    let t = (key) => i18next.t(key);
-        let contentData = [
-            {
-                'title': 'how to live an interesting live?',
-                'message': 'Live out each day. Each new day gives us opportunities and new chances'
-            },
-            {
-                'title': '5 ways to slow down',
-                'message': 'Refuse multitask How to manage everything in the furious pace of a big city'
-            },
-            {
-                'title': '8 ways to read more and frequently',
-                'message': 'In order to read more it is necessary to find more free time',
-            },
+const Useful = ({t}) => {
 
-            {
-                'title': 'How to make a good rest at the weekend?',
-                'message': 'The weekend should be planned in advance What is a weekend without a plan',
-            },
+    let contentData = [
+        {
+            'title': 'how to live an interesting live?',
+            'message': 'Live out each day. Each new day gives us opportunities and new chances'
+        },
+        {
+            'title': '5 ways to slow down',
+            'message': 'Refuse multitask How to manage everything in the furious pace of a big city'
+        },
+        {
+            'title': '8 ways to read more and frequently',
+            'message': 'In order to read more it is necessary to find more free time',
+        },
 
-            {
-                'title': 'How to become a successful businessman',
-                'message': 'Entrepreneurship involves a combination of several useful qualities',
-            },
+        {
+            'title': 'How to make a good rest at the weekend?',
+            'message': 'The weekend should be planned in advance What is a weekend without a plan',
+        },
 
-            {
-                'title': 'Habits that make us weak',
-                'message': 'Strong people are not supermen who don’t have any disadvantages',
-            },
+        {
+            'title': 'How to become a successful businessman',
+            'message': 'Entrepreneurship involves a combination of several useful qualities',
+        },
 
-            {
-                'title': 'Some tips on meditation for beginners',
-                'message': 'Mediation is a way to release from stress, obsessions and black thoughts',
-            },
+        {
+            'title': 'Habits that make us weak',
+            'message': 'Strong people are not supermen who don’t have any disadvantages',
+        },
 
-            {
-                'title': 'The best rules for personal effectiveness',
-                'message': 'Rule 1. Do not try to fill all your free time with work',
-            },
+        {
+            'title': 'Some tips on meditation for beginners',
+            'message': 'Mediation is a way to release from stress, obsessions and black thoughts',
+        },
 
-            {
-                'title': 'Clouded mind: a parable about how to get along  with your mother-in-law.',
-                'message': 'A long time ago there was a girl named Lee-Lee who got married and began to live',
-            },
-        ];
+        {
+            'title': 'The best rules for personal effectiveness',
+            'message': 'Rule 1. Do not try to fill all your free time with work',
+        },
 
+        {
+            'title': 'Clouded mind: a parable about how to get along  with your mother-in-law.',
+            'message': 'A long time ago there was a girl named Lee-Lee who got married and began to live',
+        },
+    ];
 
-        return (
-            <div className={s.main}>
-               <HeaderUsful/>
-                    {contentData.map(i =>
-                        <div className={s.container}>
-                            <details className={s.dropdownBox}>
-                                <summary className={s.dropdown}>
-                                    {t(i.title)}
-                                </summary>
-                                <p className={s.dropdownMassage}>{t(i.message)}</p>
-                            </details>
+    const dropdown = () => contentData.map(i =>
 
+            <details className={s.dropdownBox}>
+                <summary className={s.dropdownTitle}><h3>{t(i.title)}</h3></summary>
+                <p className={s.dropdownMassage}>{t(i.message)}</p>
+            </details>
 
-                            <div className={s.botContent}>
+    )
 
-                            </div>
-                        </div>
-                    )}
+    return (
+        <div className={s.useful}>
+            <img className={s.bgImage} src={faq} alt=""/>
+            <h1>useful</h1>
+            {dropdown()}
+        </div>
+    )
+};
 
-
-            </div>
-        )
-
-    }
-;
-
-export default withNamespaces()( Useful);
+export default withNamespaces()(Useful);
