05bda65c2062dff230d079c9f131f837e0742616
Ivan <zeesnippi@gmail.com>
2021-11-05 19:41:26 +0200

    Menu icons changed

--- dot_circle/src/redux/menuReducer.js
+++ dot_circle/src/redux/menuReducer.js
@@ -2,13 +2,13 @@
 const initialState = {
     headerItems: [
         {title: 'About', icon: 'bx bx-grid-alt', path: '/'},
-        {title: 'Business trainings', icon: 'bx bx-user', path: '/trainings'},
-        {title: 'Open programs', icon: 'bx bx-chat', path: '/open_programs'},
-        {title: 'Dream team', icon: 'bx bx-folder', path: '/team'},
-        {title: 'Consulting', icon: 'bx bx-cart-alt', path: '/consulting'},
-        {title: 'Webinars', icon: 'bx bx-heart', path: '/webinars'},
-        {title: 'Useful', icon: 'bx bx-cog', path: '/useful'},
-        {title: 'Gallery', icon: 'bx bx-cog', path: '/gallery'},
+        {title: 'Business trainings', icon: 'bx bx-task', path: '/trainings'},
+        {title: 'Open programs', icon: 'bx bx-conversation', path: '/open_programs'},
+        {title: 'Dream team', icon: 'bx bx-group', path: '/team'},
+        {title: 'Consulting', icon: 'bx bx-user-voice', path: '/consulting'},
+        {title: 'Webinars', icon: 'bx bx-microphone', path: '/webinars'},
+        {title: 'Useful', icon: 'bx bx-book-bookmark', path: '/useful'},
+        {title: 'Gallery', icon: 'bx bx-images', path: '/gallery'},
     ]
 }
 
