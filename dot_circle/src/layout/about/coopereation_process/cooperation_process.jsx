65266e9a49fa96a04217f207d2918d154a244794
Ivan <zeesnippi@gmail.com>
2021-11-06 11:07:33 +0200

    Cooperation process component code refactoring

--- dot_circle/src/layout/about/coopereation_process/cooperation_process.jsx
+++ dot_circle/src/layout/about/coopereation_process/cooperation_process.jsx
@@ -7,65 +7,43 @@ const CooperationProcess = ({t}) => {
     return (
 
         <div className={style.container}>
-            <h1>{t('How is the process of cooperation')}</h1>
+            <h1 className={style.cardTitle}>{t('How is the process of cooperation')}</h1>
 
             <div className={style.topLine}>
                 <div className={style.item}>
                     <h1>1</h1>
                     <img src={arrow} alt=""/>
+                    <p>{t('Training Information')}</p>
                 </div>
 
                 <div className={style.item}>
                     <h1>2</h1>
                     <img src={arrow} alt=""/>
+                    <p>{t('Personal meeting')}</p>
                 </div>
 
                 <div className={style.item}>
                     <h1>3</h1>
                     <img src={arrow} alt=""/>
+                    <p>{t('Timing')}</p>
                 </div>
 
                 <div className={style.item}>
                     <h1>4</h1>
                     <img src={arrow} alt=""/>
+                    <p>{t('Contract')}</p>
                 </div>
 
                 <div className={style.item}>
                     <h1>5</h1>
                     <img src={arrow} alt=""/>
-                </div>
-
-                <div className={style.item}>
-                    <h1>6</h1>
-                </div>
-
-            </div>
-
-            <div className={style.botLine}>
-                <div className={style.item}>
-                    <p>{t('Training Information')}</p>
-                </div>
-
-                <div className={style.item}>
-                    <p>{t('Personal meeting')}</p>
-                </div>
-
-                <div className={style.item}>
-                    <p>{t('Timing')}</p>
-                </div>
-
-                <div className={style.item}>
-                    <p>{t('Contract')}</p>
-                </div>
-
-                <div className={style.item}>
                     <p>{t('Training')}</p>
                 </div>
 
                 <div className={style.item}>
+                    <h1>6</h1>
                     <p>{t('Summary')}</p>
                 </div>
-
             </div>
 
         </div>
