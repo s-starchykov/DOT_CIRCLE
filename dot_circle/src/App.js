8a827ec6b1ec7a73b589c488511d76e4e5e76a01
Sergey <starchykov.sergey@outlook.com>
2021-11-08 13:40:19 +0200

    Component DreamTeam

--- dot_circle/src/App.js
+++ dot_circle/src/App.js
@@ -6,6 +6,7 @@ import WelcomePage from "./layout/welcome_page/welcome_page";
 import NoPage from "./layout/common/no_page/no_page";
 import Useful from "./layout/useful/useful";
 import Webinars from "./layout/webinars/webinars";
+import DreamTeam from "./layout/dream_team/dream_team";
 
 
 
@@ -18,7 +19,7 @@ const App = () => {
                 <Route exact path={'/'} render={() => <About/>}/>
                 <Route exact path={'/trainings'} render={() => <NoPage name={'Business trainings'}/>}/>
                 <Route exact path={'/open_programs'} render={() => <NoPage name={'Open programs'}/>}/>
-                <Route exact path={'/team'} render={() => <NoPage name={'Team'}/>}/>
+                <Route exact path={'/team'} render={() => <DreamTeam name={'Team'}/>}/>
                 <Route exact path={'/consulting'} render={() => <NoPage name={'Consulting'}/>}/>
                 <Route exact path={'/webinars'} render={() => <Webinars name={'Webinars'}/>}/>
                 <Route exact path={'/useful'} render={() => <Useful name={'Useful'}/>}/>
---
+++ dot_circle/src/layout/dream_team/dream_team.jsx
@@ -0,0 +1,56 @@
+import s from "./dream_team.module.scss"
+import {useState} from "react";
+import {withNamespaces} from "react-i18next";
+import executiveDirector from '../../assets/Anar_dream_team.png'
+import trainingAssistant from '../../assets/Zulfia_dream_team.png'
+import challenger from '../../assets/Nurtac_dream_team.png'
+import dreamTeam from "../../assets/dream_team.png";
+
+
+const DreamTeam = ({t}) => {
+    const data = [
+        {
+            name: 'Anar Mammadov',
+            position: 'Executive Director/ Co-founder',
+            image: executiveDirector,
+
+        },
+        {
+            name: 'Zulfiya Nagiyeva',
+            position: "Training Assistant",
+            image: trainingAssistant,
+
+        },
+        {
+            name: 'Guliyeva Nurtac',
+            position: "Challenger",
+            image: challenger,
+
+        }
+    ]
+    let card = data.map(i =>
+        <div className={s.card}>
+            <img src={i.image} alt=""/>
+            <div className={s.description}>
+                <h3>
+                    {t(i.name)}
+                </h3>
+                <p>
+                    {t(i.position)}
+                </p>
+            </div>
+
+        </div>)
+    return (
+        <div className={s.dreamTeam}>
+            <img className={s.bgImage} src={dreamTeam} alt=""/>
+            <h1> Dream team</h1>
+
+            {card}
+
+        </div>
+    )
+}
+
+
+export default withNamespaces()(DreamTeam);
