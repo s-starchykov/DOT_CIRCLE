fb0cc805f04eb5f679b6a5f20b4cef546bc40dd3
Ivan <zeesnippi@gmail.com>
2021-12-01 19:15:59 +0200

    Custom background component

---
+++ dot_circle/src/layout/common/background/background.jsx
@@ -0,0 +1,22 @@
+const Background = (props) => {
+
+    const background = {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+
+        height: '100%',
+        width: '100%',
+
+        backgroundImage: `url(${props.background})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: 'fixed'
+    };
+
+    return <div style={background}/>
+};
+
+export default Background;
