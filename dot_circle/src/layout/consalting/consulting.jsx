5e2ec883bb91fa8f399eb6be01a9b3ceccac47b4
Ivan <zeesnippi@gmail.com>
2021-12-02 00:19:06 +0200

    Consulting page fixed

--- dot_circle/src/layout/consalting/consulting.jsx
+++ dot_circle/src/layout/consalting/consulting.jsx
@@ -1,48 +1,48 @@
 import s from "./welcome_consalting.module.scss"
 import {withNamespaces} from "react-i18next";
 import consulting from "../../assets/consalting.png"
-import {NavLink, Route, useRouteMatch} from "react-router-dom";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Iframe from "../common/iframe/iframe";
 import IndividualConsulting from "./individual_consalting/individual_consalting";
+import CustomPopup from "../common/popup/popup";
+import Background from "../common/background/background";
+import PageTitle from "../common/page_title/page_title";
 
 const Consulting = ({t}) => {
-    const match = useRouteMatch();
-    const [showManagement, setShowManagement] = useState(false);
+
+    // Set document title wia hook effect
+    useEffect(() => document.title = t('Consulting'));
+
+    const [showPopup, setShowPopup] = useState(false);
     const [showIndividual, setShowIndividual] = useState(false);
 
+
     return (
-        <div className={s.consultingPage}>
-            <img src={consulting} alt="" className={s.bgImage}/>
-            <h1 className={s.pageTitle}>{t('Individual consulting')}</h1>
-            <div className={s.consultingType}
-                 onClick={() => setShowIndividual(!showIndividual)}>
-                <div>
+        <div className={`${s.consultingPage} ${showIndividual && s.mute} ${showPopup && s.mute}`}>
+            <Background background={consulting}/>
+            <PageTitle title={t('Consulting')}/>
+
+            <div className={s.consultingType} onClick={() => setShowIndividual(!showIndividual)}>
                     <h1>{t('Individual consulting')}</h1>
                     <p>{t('Discover your talents')}</p>
-                </div>
             </div>
 
-            {showIndividual && <div className={s.individualConsulting}>
-                <button onClick={() => setShowIndividual(!showIndividual)}>X</button>
-
-            </div>}
+            <CustomPopup onClose={() => setShowIndividual(!showIndividual)}
+                         show={showIndividual}
+                         title="Hello Jeetendra"
+                         children={<IndividualConsulting/>}/>
 
             <i className={'bx bx-sort-alt-2'}/>
 
-
-            <div className={s.consultingType} onClick={() => setShowManagement(!showManagement)}>
-                <div>
+            <div className={s.consultingType} onClick={() => setShowPopup(!showPopup)}>
                     <h1>{t('Management consulting')}</h1>
                     <p>{t('www.altempuscapital.com')}</p>
-                </div>
+            </div>
 
-            </div>
-            {showManagement && <div className={s.appear}>
-                <button onClick={() => setShowManagement(!showManagement)}>x</button>
-                <Iframe source={'http://dot-circle.com/pages/consulting.html'}/>
-            </div>
-            }
+            <CustomPopup onClose={() => setShowPopup(!showPopup)}
+                         show={showPopup}
+                         title={t('Management consulting')}
+                         children={<Iframe source={'https://dot-circle.com/pages/consulting.html'}/>}/>
         </div>
     )
 }
